0b9187961a12eede32de87a53937fb0f39648e3b
vladtymo <tymo.vlad@gmail.com>
2022-07-06 15:38:53 +0300

    Create User model, User Controller and User list view.

---
+++ FirstAspNetMvc_project/Controllers/UserController.cs
@@ -0,0 +1,25 @@
+﻿using FirstAspNetMvc_project.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FirstAspNetMvc_project.Controllers
+{
+    public class UserController : Controller
+    {
+        private List<User> users = new List<User>();
+        public UserController()
+        {
+            users.Add(new User() { Name = "Vlad", Surname = "Tymo", Email = "rejv434@gmail.com", BirthDate = DateTime.Now });
+            users.Add(new User() { Name = "Bob", Surname = "Bobovich", Email = "super4344@gmail.com", BirthDate = DateTime.Now });
+            users.Add(new User() { Name = "Igor", Surname = "Rufer", Email = "hgkkkkff@gmail.com", BirthDate = DateTime.Now });
+        }
+
+        public IActionResult Index()
+        {
+            return View(users);
+        }
+    }
+}
---
+++ FirstAspNetMvc_project/Models/User.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FirstAspNetMvc_project.Models
+{
+    public class User
+    {
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public string Email { get; set; }
+        public DateTime BirthDate { get; set; }
+    }
+}
